78abe6dec4365489bdf9921f173bc78845c96a42
HeidiHettiarachchi <it22323248@my.sliit.lk>
2026-03-08 02:37:30 +0530

    fixed casing

--- exoplanet/frontend/src/App.tsx
+++ exoplanet/frontend/src/App.tsx
@@ -1,27 +1,18 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import viteLogo from '/vite.svg';
 import Home from '../pages/Home';
-import Header from '../pages/component/header';
+import Header from '../pages/component/Header'; // fix casing!
 import About from '../pages/About';
 import ContactUs from '../pages/ContactUs';
 import Simulation from '../pages/Simulation';
 import ComponentPage from '../pages/ComponentPage';
 import Login from '../pages/Login';
-import ExoDetect from '../pages/ExoDetection';
-import ExtraMineral from '../pages/ExtraMineral';
-import AtmosProfile from '../pages/AtmosProfile';
-import StellarAnalysis from '../pages/StellarAnalysis';
 import './App.css';
 
 function App() {
   return (
-
-    
     <Router>
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -29,14 +20,9 @@ function App() {
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/login" element={<Login />} />
         <Route path="/services" element={<ComponentPage />} />
-        <Route path="/exoplanetDetection" element={<ExoDetect />} />
-        <Route path="/extraMineral" element={<ExtraMineral />} />
-        <Route path="/atmosphereProfile" element={<AtmosProfile />} />
-        <Route path="/stellarAnalysis" element={<StellarAnalysis />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App
